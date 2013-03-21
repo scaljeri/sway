00ea02a0f020d72ab1db81733719e277f8d3442f
Lucas Calje <lucas@calje.eu>
2013-03-21 07:47:19 +0100

    added project documentation

---
+++ api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Sway",
+        "Sway.DI",
+        "Sway.EventHub",
+        "Sway.data.Field",
+        "Sway.data.Record",
+        "Sway.filter.Compress",
+        "Sway.filter.Encrypt"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
