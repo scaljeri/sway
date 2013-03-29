1fdaa92e90ab33fc8b15b33968e62008bcd18c06
Lucas Calje <lucas@calje.eu>
2013-03-29 15:42:30 +0100

    travis check

--- testacular.conf.js
+++ testacular.conf.js
@@ -71,7 +71,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['Zombie'];
+browsers = ['PhantomJS'];
 
 
 // If browser does not capture in given timeout [ms], kill it
