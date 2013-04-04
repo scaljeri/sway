dbe54ceb78c8130f40bd5a82743c58630a1c1a50
jeanluca <lucas@calje.eu>
2013-04-04 20:43:19 +0200

    new site

---
+++ yuidoc/api.js
@@ -0,0 +1,15 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Sway",
+        "Sway.DI",
+        "Sway.EventHub",
+        "Sway.data.ActiveRecord.",
+        "Sway.data.Field",
+        "Sway.filter.Compress",
+        "Sway.filter.Encrypt"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
