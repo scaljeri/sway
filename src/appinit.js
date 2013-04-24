8f687d90e5e6d8fce1915db182a395681b5d74c8
jeanluca <lucas@calje.eu>
2013-04-24 21:49:58 +0200

    file missing for code-coverage report

---
+++ src/appinit.js
@@ -0,0 +1,26 @@
+/**
+ * Namespace
+ * @class Sway
+ */
+window.Sway = window.Sway || {} ;
+
+/**
+  * Version of the framework
+  * @property VERSION
+  * @type String
+  **/
+window.Sway.VERSION = '1.0alpha' ;
+
+/**
+  * If TRUE, debugging is enabled. In PRODUCTION this property should be set to FALSE!!
+  * @property DEBUG
+  * @type boolean
+  **/
+window.Sway.DEBUG = true ;
+
+// zip.js settings
+if ( !window.zip ){
+    window.zip = {} ;
+}
+zip.useWebWorkers     = true ;
+zip.workerScriptsPath = "lib/zip.js/" ;
