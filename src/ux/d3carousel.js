f826f877422d73c5b17408be7c0bf9e8fe238e1b
jeanluca <lucas@calje.eu>
2013-05-10 08:50:03 +0200

    UX - 3d carousel idea

---
+++ src/ux/d3carousel.js
@@ -0,0 +1,15 @@
+// Create the namespace -> JS load order independent
+window.Sway = window.Sway || {} ;
+window.Sway.ux = window.Sway.ux || {} ;
+
+(function(ns){
+    /**
+     * @class D3Carousel
+     * @constructor
+     */
+    var D3Carousel = function(){
+
+   } ;
+
+   ns.D3Carousel = D3Carousel ;
+})(window.Sway.ux) ;
