3ff748c51ae40e7a64a63348179d4b03f30875af
jeanluca <lucas@calje.eu>
2013-05-22 22:17:47 +0200

    fixed style issues

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,9 +9,21 @@ $(function(){
     $('.menu-item').on('click', showContent) ;
 
     function showContent(e) {
+        var iframe = $('#item-frame') ;
+
         var url = $(this).attr('data-href') ;
-        $('#item-frame').attr('src', url)
-            .animate( { top: 20, height: '100%'}) ;
+        if ( !url ) {
+            iframe.attr('src', url)
+                .animate( { top: 100, height: 0}) ;
+        }
+        else {
+            // make sure the background is white (and not transparent)
+            iframe.attr('src', url)
+                .animate( { top: 20, height: '100%'})
+                .load(function() {
+                    this.contentDocument.body.style.backgroundColor = '#fff' ;
+                }) ;
+        }
 
         /*
          //parent.frames[FrameID].window.location.reload();
