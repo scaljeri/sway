ebcd25296d440460d28f107e9c45864a22528bf5
jeanluca <lucas@calje.eu>
2013-07-13 22:58:32 +0200

    doc (WIP)

--- src/data/field.js
+++ src/data/field.js
@@ -21,6 +21,7 @@ window.Sway.data = window.Sway.data || {};
      *      <li>has_many through</li>
      *      <li>has_and_belongs_to_many</li>
      * </ul>
+     * Between two tables only one relation can be defined!
      *
      * Furthermore, a Field can perform data validation and transformation
      *
@@ -30,11 +31,11 @@ window.Sway.data = window.Sway.data || {};
      * <h3>Associations</h3>
      *
      * This Active Record implementation is based on the Ruby on Rails (RoR) <a href="http://guides.rubyonrails.org/association_basics.html">ActiveRecord Associations</a>.<br>
-     * The following associations are available:
+     * The following associations are supported:
      *
      * <h4>BELONGS_TO and HAS_ONE (THROUGH) Association</h4>
      * Assume an 'Account' <tt>belongs\_to</tt> a 'Supplier'. Or the other way around, a Supplier <tt>has\_one</tt> Account.
-     * Furthermore, if an 'Account' and 'Supplier' both <tt>has\_one</tt> 'AccountHistory', it could look like this
+     * And, if an 'Account' and 'Supplier' both <tt>has\_one</tt> 'AccountHistory', it could look like this
      *
      *      var Account = new Sway.data.ActiveRecord( 'Account', storage, [
      *            new Sway.data.Relation({ key: 'supplier', type: 'belongs_to', model: 'Supplier' }       // creates a field 'supplier_id'
@@ -44,7 +45,7 @@ window.Sway.data = window.Sway.data || {};
      *
      *      var Supplier = new Sway.data.ActiveRecord( 'Supplier', storage, [
      *           new Sway.data.Relation({ key: 'account', type: 'has_one', model: 'Account' })
-     *           new Sway.data.Relation({ key: 'accountHistory', type: 'has_one', through: 'AccountHistory', model: 'Account' })
+     *           new Sway.data.Relation({ key: 'accountHistory', type: 'has_one', through: 'Account', model: 'AccountHistory' })
      *           ....
      *      ]) ;
      *
@@ -54,12 +55,18 @@ window.Sway.data = window.Sway.data || {};
      *      ]) ;
      *
      *      ...
-     *      supplierRecord.accountHistory = someAccountHistories
      *
-     * An <tt>belongs_to</tt> field is always required!!
+     * And the initialization process goes as follows:
+     *
+     *     var account = new Account( { ... })
+     *       , accountHistory = new AccountHistory( { ... } )
+     *       , supplier = new Supplier({..}) ;
+     *
+     *     account.accountHistory = accountHistory ;
+     *     supplier.accountHistory = accountHistory ;
      *
      * <h4>HAS_MANY (THROUGH)</h4>
-     * This association define a one-to-many or a many-to-many relationship.
+     * This association defines a one-to-many or a many-to-many relationship.
      *
      *      new Sway.data.Relation({ key: 'orders', type: 'has_many', model: 'Order' }) ;            // one-to-many
      *      new Sway.data.Relation({ key: 'patients', type: 'has_many', through: 'Appointment', model: 'Patient' }) ;  // many-to-many
@@ -79,85 +86,17 @@ window.Sway.data = window.Sway.data || {};
      * @param {String} key name of the field
      * @param {Object} [options] definition of this field
      *      @param {String}  [options.type=TEXT] type of the field or association
-     *      @param {String}  [options.model] referenced model
-     *      @param {String}  [options.through] specifies a join model. Only available for <tt>HAS\_ONE</tt> and <tt>HAS\_MANY</tt> associations
+     *      @param {String}  [options.model] referenced model (only for associations)
+     *      @param {String}  [options.through] specifies a join model. Only available for <tt>HAS\_ONE</tt> and <tt>HAS\_MANY</tt> associations (only for associations)
      *      @param {String}  [options.friendlyName] description of the field
-     *      @param {Boolean} [options.PK=false] primary key field (there can only be one primary key field)
+     *      @param {Boolean} [options.PK=false] primary key field (there can only be one primary key field). If not defined, one will be created.
      *      @param {Boolean} [options.autoIncrement=true] Primary key field is auto-incremented (auto generated key)
      *      @param {Boolean} [options.index=false] put an index on the field
      *      @param {Boolean} [options.unique=false] unique field
      *      @param {Array}   [options.compoundIndex] names of the compound index it is part of.
      *      @param {Boolean} [options.required=false] a required field
-     *      @param {Array}   [options.transformers] list of transformer objects. A transformer object can transform the data into a new form and also back
-     * into its original form. Think of, zipping and unzipping or encrypting and decrypting
-     *      @param {Array}   [options.validators] list of validation functions
-     */
-
-    /*
-     *) Rule nr1: Only one relation can exist between two tables
-
-     belongs_to ( === foreign key ) (http://stackoverflow.com/questions/3808926/whats-the-difference-between-belongs-to-and-has-one)
-     has_one
-     has_many
-     has_many :through   (the thirt table is also a model)
-     has_one :through    ( goes one-to-one through other model)
-     has_and_belongs_to_many    // simple linker table (http://stackoverflow.com/questions/2780798/has-and-belongs-to-many-vs-has-many-through)
-     */
-
-    /**
-     * Sway.data.Relation defines the association between two {{#crossLink "Sway.data.Model"}}{{/crossLink}}s. It is based on the Ruby on Rails (RoR)
-     * <a href="http://guides.rubyonrails.org/association_basics.html">ActiveRecord Associations</a>.<br>
-     * The following associations are available:
-     *
-     * <h4>BELONGS_TO and HAS_ONE (THROUGH) Association</h4>
-     * Assume an 'Account' <tt>belongs\_to</tt> a 'Supplier'. Or the other way around, a Supplier <tt>has\_one</tt> Account.
-     * Furthermore, if an 'Account' and 'Supplier' both <tt>has\_one</tt> 'AccountHistory', it will look like this
-     *
-     *      var Account = new Sway.data.ActiveRecord( 'Account', storage, [
-     *            new Sway.data.Relation({ key: 'supplier', type: 'belongs_to', model: 'Supplier' }       // creates a field 'supplier_id'
-     *            new Sway.data.Relation({ type: 'has_one', model: 'AccountHistory' }                     // AccountHistory should have a account_id field
-     *            .....
-     *      ]) ;
-     *
-     *      var Supplier = new Sway.data.ActiveRecord( 'Supplier', storage, [
-     *           new Sway.data.Relation({ key: 'account', type: 'has_one', model: 'Account' })
-     *           new Sway.data.Relation({ key: 'accountHistory', type: 'has_one', through: 'AccountHistory', model: 'Account' })
-     *           ....
-     *      ]) ;
-     *
-     *      var AccountHistory = new Sway.data.ActiveRecord( 'AccountHistory', storage, [
-     *          new Sway.datat.Relation({ type: 'belongs_to', model: 'Account' })                          // creates an 'account_id' field
-     *          ...
-     *      ]) ;
-     *
-     *      ...
-     *      supplierRecord.accountHistory = someAccountHistories
-     *
-     * An <tt>belongs_to</tt> field is always required!!
-     *
-     * <h4>HAS_MANY (THROUGH)</h4>
-     * This association define a one-to-many or a many-to-many relationship.
-     *
-     *      new Sway.data.Relation({ key: 'orders', type: 'has_many', model: 'Order' }) ;            // one-to-many
-     *      new Sway.data.Relation({ key: 'patients', type: 'has_many', through: 'Appointment', model: 'Patient' }) ;  // many-to-many
-     *
-     * <h4>HAS_AND_BELONGS_TO_MANY</h4>
-     * This relation is identical to <tt>HAS\_MANY THROUGH</tt> (many-to-many), except it doesn't have a third model
-     *
-     *      new Sway.data.Relation({ key: 'assemblies', type: 'has_and_belongs_to_many', model: 'Assembly' })
-     *
-     * And the association in the other moddel look like
-     *
-     *      new Sway.data.Relation({ key: 'parts', type: 'has_and_belongs_to_many', model: 'Part' })
-     *
-     * @class Sway.data.Relation
-     * @constructor
-     * @param {String} key
-     * @param {String} type type of association
-     * @param {String} model name of the associated model
-     * @param {Object} [options]
-     *          @param {String} [options.through] specifies a join model. Only available for <tt>HAS\_ONE</tt> and <tt>HAS\_MANY</tt> associations
-     *          @param {String} [options.friendlyName] description of the field
+     *      @param {Array}   [options.transformers] list of transformer objects. TODO
+     *      @param {Array}   [options.validators] list of validation functions. TODO
      */
     var Field = function (key, options) {
         if ( !options ) {
@@ -192,8 +131,8 @@ window.Sway.data = window.Sway.data || {};
                 this.set = setHasMany.bind(this);
                 this.defaultValue = options.defaultValue||[] ;
                 break;
-            default: // its not a association
-                if ( this.model || this.through ) {
+            default: // its not an association
+                if ( this.model || this.through ) { // check input
                     throw "The field " + key + " does not support the mode/through property (" + this.model + " " + this.through + ")" ;
                 }
         }
@@ -290,7 +229,8 @@ window.Sway.data = window.Sway.data || {};
          * @method validate
          * @param {*} value value to be validated
          * @return {Boolean}
-         */, validate: function (value) {
+         */
+        , validate: function (value) {
             var i
                 , ok = true;
 
